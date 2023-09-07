2e691c77ffcb6298974a1af8b851e67af1bee9e2
Priyanshu-IIT-IIM <priyanshu.jain32@gmail.com>
2023-09-07 12:34:42 +0530

    v-on directive changes color of button upon action

--- project_1/moreVue3.js
+++ project_1/moreVue3.js
@@ -4,13 +4,24 @@ const app = createApp({
 	data() {
 		return {
 			message: "Say What? ",
-			bgColor: 0
+			bgColor: 0,
+			btnClass: "btn-primary"
 		}
 	},
 	methods: {
 		changeTheme() {
-			this.bgColor += 50
+			this.bgColor += 50;
+		},
+		btnColor() {
+			if (this.btnClass === "btn-primary") {
+				this.btnClass = "btn-secondary";	
+			}
+			else {
+				this.btnClass = "btn-primary";
+			}
+			
 		}
 	}
+
 })
 app.mount('#app')
